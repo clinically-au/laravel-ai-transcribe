e64ba82732eb5e5a9857080389374bbb533eb761
Wojt Janowski <wojt@clinically.com.au>
2026-06-12 12:28:04 +1000

    fix: use defineEnvironment instead of deprecated getEnvironmentSetUp

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,7 +17,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         ];
     }
 
-    protected function getEnvironmentSetUp($app): void
+    protected function defineEnvironment($app): void
     {
         $app['config']->set('ai.providers.aws', [
             'driver' => 'aws-transcribe',
