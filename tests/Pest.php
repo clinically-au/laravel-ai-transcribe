d4cca72a05c70f28430ba9a0e77ef497b05b20b2
Wojt Janowski <wojt@clinically.com.au>
2026-06-12 12:21:36 +1000

    chore: scaffold package skeleton

---
+++ tests/Pest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+use Aws\CommandInterface;
+use Aws\MockHandler;
+use Aws\S3\S3Client;
+use Aws\TranscribeService\TranscribeServiceClient;
+use Clinically\AiTranscribe\TranscribeProvider;
+
+uses(Clinically\AiTranscribe\Tests\TestCase::class)->in('Unit', 'Feature');
+
+/**
+ * Build a MockHandler that records each command's name and params
+ * into $commands and returns the queued results in order.
+ *
+ * @param  array<int, \Aws\ResultInterface|\Aws\Exception\AwsException>  $results
+ * @param  array<int, array{name: string, params: array<string, mixed>}>  $commands
+ */
+function mockedHandler(array $results, array &$commands): MockHandler
+{
+    $handler = new MockHandler;
+
+    foreach ($results as $result) {
+        $handler->append(function (CommandInterface $command) use (&$commands, $result) {
+            $commands[] = ['name' => $command->getName(), 'params' => $command->toArray()];
+
+            return $result;
+        });
+    }
+
+    return $handler;
+}
+
+function mockedS3(MockHandler $handler): S3Client
+{
+    return new S3Client([
+        'region' => 'ap-southeast-2',
+        'version' => 'latest',
+        'handler' => $handler,
+        'credentials' => ['key' => 'test', 'secret' => 'test'],
+    ]);
+}
+
+function mockedTranscribe(MockHandler $handler): TranscribeServiceClient
+{
+    return new TranscribeServiceClient([
+        'region' => 'ap-southeast-2',
+        'version' => 'latest',
+        'handler' => $handler,
+        'credentials' => ['key' => 'test', 'secret' => 'test'],
+    ]);
+}
+
+/**
+ * @param  array<string, mixed>  $overrides
+ */
+function makeProvider(array $overrides = []): TranscribeProvider
+{
+    return new TranscribeProvider(array_merge([
+        'driver' => 'aws-transcribe',
+        'name' => 'aws',
+        'region' => 'ap-southeast-2',
+        'bucket' => 'test-bucket',
+        'prefix' => 'transcriptions',
+        'language' => 'en-AU',
+        'access_key_id' => 'test-key',
+        'secret_access_key' => 'test-secret',
+    ], $overrides), app('events'));
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Clinically\AiTranscribe\Tests;
+
+use Clinically\AiTranscribe\AiTranscribeServiceProvider;
+use Laravel\Ai\AiServiceProvider;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            AiServiceProvider::class,
+            AiTranscribeServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('ai.providers.aws', [
+            'driver' => 'aws-transcribe',
+            'region' => 'ap-southeast-2',
+            'bucket' => 'test-bucket',
+            'prefix' => 'transcriptions',
+            'language' => 'en-AU',
+            'access_key_id' => 'test-key',
+            'secret_access_key' => 'test-secret',
+        ]);
+
+        $app['config']->set('ai.default_for_transcription', 'aws');
+    }
+}
